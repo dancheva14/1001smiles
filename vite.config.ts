ec718721cdc69d2467eacaf00e95369ca627f4cc
Iavor Orlyov <yavor.orlyov@microinvest.net>
2026-03-19 14:36:15 +0200

    Промени по билда

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Required for GitHub Pages: use your repo name (e.g. '1001smiles'). Use '/' for user/org pages.
-  base: '/1001smiles/',
-})
+  base: "/",
+});
